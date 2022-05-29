8168039c778414579adaa5874e4201e13c1f191f
Nicolas Saturno <nicolas.saturno@hotmail.com>
2022-05-29 15:29:26 -0300

    hola

--- app_coder/admin.py
+++ app_coder/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from app_coder.models import Course, Student, Profesor, Homework
+from app_coder.models import Seed, Insurance, Pipes
 
-admin.site.register(Course)
+admin.site.register(Pipes)
 
-admin.site.register(Student)
+admin.site.register(Insurance)
 
-admin.site.register(Profesor)
+admin.site.register(Seed)
 
-admin.site.register(Homework)
+
